5c3eebfef816ca28ba7e1b9ccb394093168c0ebb
Nishan Premakumara <aknpremakumara@gmail.com>
2023-04-21 09:46:39 +0530

    Url Updates

--- src/components/landingpage/FanSignUp.jsx
+++ src/components/landingpage/FanSignUp.jsx
@@ -8,7 +8,7 @@ const FanSignUp = () => {
           whileHover={{ scale: 1.1 }}
           transition={{ type: "spring", stiffness: 400, damping: 10 }}
         >
-          <a href="https://system.musicalbead.com/customer" className="relative w-fit after:items-end after:justify-end after:content-[''] after:bg-teal after:absolute after:h-[3px] after:w-[0%] after:left-0 after:-bottom-2 after:rounded-xl after:duration-300 hover:after:w-full">Sign Me Up Now</a>
+          <a href="/signup" className="relative w-fit after:items-end after:justify-end after:content-[''] after:bg-teal after:absolute after:h-[3px] after:w-[0%] after:left-0 after:-bottom-2 after:rounded-xl after:duration-300 hover:after:w-full">Sign Me Up Now</a>
         </motion.div>
       </div>
     </div>
