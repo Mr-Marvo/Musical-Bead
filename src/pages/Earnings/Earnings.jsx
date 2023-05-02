6fde9f80f0d2945a8e02dbd22270779baa2bb4f3
RuvindaTharaka <ruvindapeiris23@gmail.com>
2023-05-02 23:14:36 +0530

    earnings not completely responsive

--- src/pages/Earnings/Earnings.jsx
+++ src/pages/Earnings/Earnings.jsx
@@ -45,6 +45,12 @@ function Earnings() {
         setIsCollapsed3(!isCollapsed3);
     };
 
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let monthIndex = (new Date().getMonth());
+    let monthName = monthNames[monthIndex];
+    let year = (new Date().getFullYear());
+    let day = monthName+", "+year;
+
     return ( 
         <>
              {/* Header Navigation Bar */}
@@ -86,8 +92,111 @@ function Earnings() {
 
              {/* Body Content */}
              <main>
-                <div className="earning_container1">1</div>
-                <div className="earning_container2">2</div>
+                <div className="earning_container1">
+                    <div className="earning_container1_sub1">
+                        <pre>{day}</pre>
+                    </div>
+                    <div className="earning_container1_sub2">
+                        <div className="sub2_sub1">
+                                <div>
+                                    <h1>$2300.00</h1>
+                                    <span>Total Earnings</span>
+                                </div>
+                                <div>
+                                    <h1>$500.00</h1>
+                                    <span>Total Fees</span>
+                                </div>
+                                <div>
+                                    <h1>$000.00</h1>
+                                    <span>Total Purchases</span>
+                                </div>
+                                <div>
+                                    <h1>110</h1>
+                                    <span>Total Sales</span>
+                                </div>
+                                <div>
+                                    <h1>30</h1>
+                                    <span>Total Ships</span>
+                                </div>
+                                <div>
+                                    <h1>80</h1>
+                                    <span>Shipped</span>
+                                </div>
+                            
+                        </div>
+                        <div className="sub2_sub2">
+                                <h1>$1800.00</h1>
+                                <span>Available Funds</span>
+                                <button type="button"  className='withdraw_btn'>
+                                    Withdraw
+                                </button>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="earning_container2">
+                    <div>
+                        <pre className='headline_left'>Order History</pre>
+                        <pre>Last 3 Months</pre>
+                    </div>
+                    <div className='tbl_container' style={{overflowX: 'auto'}}>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Order Number</th>
+                                    <th>Date</th>
+                                    <th>Order Status</th>
+                                    <th>Tracing Number</th>
+                                    <th>Order Details</th>
+                                </tr>  
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><img src={Singer} alt="User Image" className='singer_image' /></td>
+                                    <td>O-000-001</td>
+                                    <td>2023-03-22</td>
+                                    <td><button type="button"  className='view_btn' style={{backgroundColor:'#059f3b'}}>To Ship</button></td>
+                                    <td>-</td>
+                                    <td><button type="button"  className='view_btn'>View</button></td>
+                                </tr>
+                                <tr>
+                                    <td><img src={Singer} alt="User Image" className='singer_image' /></td>
+                                    <td>O-000-001</td>
+                                    <td>2023-03-22</td>
+                                    <td><button type="button"  className='view_btn' style={{backgroundColor:'#059f3b'}}>To Ship</button></td>
+                                    <td>-</td>
+                                    <td><button type="button"  className='view_btn'>View</button></td>
+                                </tr>
+                                <tr>
+                                    <td><img src={Singer} alt="User Image" className='singer_image' /></td>
+                                    <td>O-000-001</td>
+                                    <td>2023-03-22</td>
+                                    <td><button type="button"  className='view_btn' style={{backgroundColor:'#c5d513'}}>Shipped</button></td>
+                                    <td>TR00-001-002</td>
+                                    <td><button type="button"  className='view_btn'>View</button></td>
+                                </tr>
+                                <tr>
+                                    <td><img src={Singer} alt="User Image" className='singer_image' /></td>
+                                    <td>O-000-001</td>
+                                    <td>2023-03-22</td>
+                                    <td><button type="button"  className='view_btn' style={{backgroundColor:'gray'}}>Completed</button></td>
+                                    <td>TR00-001-002</td>
+                                    <td><button type="button"  className='view_btn'>View</button></td>
+                                </tr>
+                                <tr>
+                                    <td><img src={Singer} alt="User Image" className='singer_image' /></td>
+                                    <td>O-000-001</td>
+                                    <td>2023-03-22</td>
+                                    <td><button type="button"  className='view_btn' style={{backgroundColor:'#c5d513'}}>Shipped</button></td>
+                                    <td>TR00-001-002</td>
+                                    <td><button type="button"  className='view_btn'>View</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    
+                </div>
              </main>
 
              {/* Footer Content */}
