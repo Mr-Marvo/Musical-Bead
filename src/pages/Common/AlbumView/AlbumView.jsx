5c51fef63c70d902366f5be764578c8396d88ed0
RuvindaTharaka <ruvindapeiris23@gmail.com>
2023-05-12 11:22:06 +0530

    album_view

--- src/pages/Common/AlbumView/AlbumView.jsx
+++ src/pages/Common/AlbumView/AlbumView.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import './AlbumView.css';
 import { NewHeader,NewFooter } from '../../../components/system';
-import Album4 from '../Album4';
+
+import img1 from '../../../assets/images/system/Rectangle 139.png';
+import img2 from '../../../assets/images/system/Rectangle 140.png';
+import img3 from '../../../assets/images/system/Rectangle 141.png';
+import img4 from '../../../assets/images/system/Rectangle 142.png';
+import img5 from '../../../assets/images/system/Rectangle 143.png';
+import img6 from '../../../assets/images/system/Rectangle 144.png';
 
 function AlbumView() {
     return ( 
@@ -14,7 +20,19 @@ function AlbumView() {
                             Hip hop
                         </button>
                         <div className="album_view_container">
-                            <Album4/>
+                            <div style={{width:'400px',height:'320px',background:'green',borderRadius:'25px'}}>
+                                
+                            </div>
+
+                            <span className='songs_list font-nunito'>Songs List</span>
+
+                            <div className='songs_container'>
+                                <div className='songs_wrap'>
+                                    <img src={img1} alt="song_icon" width="60px"/>
+                                    <span>Lorem ipsum dagdahahhkjhdkad.com</span>
+                                </div>
+                            </div>
+                           
                         </div>
                     </div>
                     <div className="album_view_right_container">
